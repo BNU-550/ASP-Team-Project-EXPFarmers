ec99b135ffedb55b64dcc9a8a097eab585233093
Derek Peacock <derek.peacock@yahoo.com>
2022-01-07 19:49:29 +0000

    Scaffolded Order Items

---
+++ ASP-Razor-TeamEXPFarmers/Pages/OrderItems/Edit.cshtml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ASP_Razor_TeamEXPFarmers.Data;
+using ASP_Razor_TeamEXPFarmers.Models;
+
+namespace ASP_Razor_TeamEXPFarmers.Pages.OrderItems
+{
+    public class EditModel : PageModel
+    {
+        private readonly ASP_Razor_TeamEXPFarmers.Data.ApplicationDbContext _context;
+
+        public EditModel(ASP_Razor_TeamEXPFarmers.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public OrderItem OrderItem { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            OrderItem = await _context.OrderItems
+                .Include(o => o.Order)
+                .Include(o => o.VideoGame).FirstOrDefaultAsync(m => m.OrderItemID == id);
+
+            if (OrderItem == null)
+            {
+                return NotFound();
+            }
+           ViewData["OrderID"] = new SelectList(_context.Orders, "OrderID", "OrderID");
+           ViewData["VideoGameID"] = new SelectList(_context.VideoGames, "VideoGameID", "Title");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(OrderItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderItemExists(OrderItem.OrderItemID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool OrderItemExists(int id)
+        {
+            return _context.OrderItems.Any(e => e.OrderItemID == id);
+        }
+    }
+}
