9d33c0530ed384d86cd427e103a76fb68f6c12bc
JHuggins11 <58664223+JHuggins11@users.noreply.github.com>
2022-01-09 16:19:06 +0000

    Added some more annotations to GameImage.cs.

--- ASP-Razor-TeamEXPFarmers/Models/GameImage.cs
+++ ASP-Razor-TeamEXPFarmers/Models/GameImage.cs
@@ -7,7 +7,7 @@ namespace ASP_Razor_TeamEXPFarmers.Models
     /// caption and a link to the video game it's related to.
     /// 
     /// Created by Jason Huggins
-	/// Modified by Jason Huggins (15/12/2021)
+	/// Modified by Jason Huggins (09/01/2022)
     /// </summary>
     public class GameImage
     {
@@ -25,12 +25,13 @@ namespace ASP_Razor_TeamEXPFarmers.Models
         /// <summary>
         /// The image's file path/URL.
         /// </summary>
+        [StringLength(200), Required]
         public string URL { get; set; }
 
         /// <summary>
         /// The image's caption.
         /// </summary>
-        [StringLength(50)]
+        [StringLength(50), Required]
         public string Caption { get; set; }
 
         // Navigation properties
